6d1f0ebb0f095193c73d51dc48d2dcc20d9bb6be
Selena Small <selena@hmsnz.com>
2016-10-12 17:09:46 +1300

    active item moves to complete and changes colour

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
-    $('h1').click(function(){
-        $(this).fadeOut('slow');         
+    // Create new task
+    $('button.task-create').click(function() {
+      /// This is where I am up to .... (not documented yet) ///
+      var taskTitle = $('input[placeholder=Title]').val();
+        $('div.task-list#active').append(taskTitle).addClass('task-header');
+        
+
+
+
+        return false;
     });
-    
+
+    // Complete task
+    $('button.task-complete').click(function() {
+      var complete = $(this).parent();
+        $(this).parent().parent().next().append(complete);
+        $(this).parent().addClass('complete');
+        $(this).html("uncomplete");        
+    });
+
+    // Uncomplete a task
+
+
+    // Delete task
+    $('button.task-delete').click(function() {
+        $(this).parent().fadeOut('fast');
+    });
+
+
 });
