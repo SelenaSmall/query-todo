7cb91861aed7780f8c10c5db1067fffd5663b48b
Selena Small <selena@hmsnz.com>
2016-10-13 16:07:25 +1300

    form field and task-complete buttons validate

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
 $(document).ready(function() {
-    // Create new task
-    $('button.task-create').click(function() {
-      /// This is where I am up to .... (not documented yet) ///
-      var taskTitle = $('input[placeholder=Title]').val();
-        $('div.task-list#active').append(taskTitle).addClass('task-header');
-        
-
-
-
-        return false;
+  // Select date using jQuery date-picker
+  $('input#datepicker').focus(function() {
+    $(this).datepicker({
+      changeMonth: true,  // Allowing user to select month
+      changeYear: true    // Allowing user to select from year range
     });
+  });
 
-    // Complete task
+  // Create new task
+  $('button.task-create').click(function() {
+    var taskTitle = $('input[placeholder=Title]').val();
+    var taskDesc = $('textarea[placeholder=Description]').val();
+    var taskDate = $('input[id=datepicker]').val();
+      
+      // Validator to ensure values are not empty when creating a new task
+      if( !$('input[placeholder=Title]').val() ) { 
+        if( !$('textarea[placeholder=Description]').val() ) { 
+          if( !$('input[id=datepicker]').val() ) { 
+            alert('All values are empty');    
+          } else {
+          alert('Name & Description are epmty');    
+          }  
+        } else {
+        alert('Name is empty');    
+        }
+      } else {
+        if( !$('textarea[placeholder=Description]').val() ) { 
+          if( !$('input[id=datepicker]').val() ) { 
+            alert('Description and Date are empty');    
+          } else {
+          alert('Description is empty'); 
+          } 
+        } else {
+          // Create new #todo section
+          $('div.task-list#active').append('<div class="todo-task"></div>');
+          // Append new values to the section
+          $('div.todo-task:last-child').append(taskTitle).addClass('task-header');
+          $('div.todo-task:last-child').append(taskDesc).addClass('task-description');
+          $('div.todo-task:last-child').append(taskDate).addClass('task-due');       
+          // Create 'complete' and 'delete' buttons for the new section
+          $('div.todo-task:last-child').append('<button class="task-complete">Complete</button>');
+          $('div.todo-task:last-child').append('<button class="task-delete">Delete</button>');
+        }
+      }
+
+      // Clear 'New task' values once they have been created
+      $("#todo-form")[0].reset();
+
+    // Click complete/uncomplete task
     $('button.task-complete').click(function() {
-      var complete = $(this).parent();
-        $(this).parent().parent().next().append(complete);
+      var myTask = $(this).parent();
+      if ( $(this).parents("#active").length == 1 ) { 
+        $(this).parent().parent().next().append(myTask);
         $(this).parent().addClass('complete');
-        $(this).html("uncomplete");        
+        $(this).html("Uncomplete");
+      } else {
+        $(this).parent().parent().prev().append(myTask);
+        $(this).parent().removeClass('complete');
+        $(this).html("Complete");
+      }
     });
 
-    // Uncomplete a task
-
-
-    // Delete task
+    // Click delete task
     $('button.task-delete').click(function() {
         $(this).parent().fadeOut('fast');
-    });
+    }); 
 
+    return false;
+  });
 
 });
