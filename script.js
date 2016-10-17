51a70c4967fac6f0b214ce070d45f2ef4d79e232
Selena Small <selena@hmsnz.com>
2016-10-17 15:47:11 +1300

    styling

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ $(document).ready(function() {
     var taskTitle = $('input[placeholder=Title]').val();
     var taskDesc = $('textarea[placeholder=Description]').val();
     var taskDate = $('input[id=datepicker]').val();
-      
-      // Validator to ensure values are not empty when creating a new task
-      if( !$('input[placeholder=Title]').val() ) { 
-        if( !$('textarea[placeholder=Description]').val() ) { 
-          if( !$('input[id=datepicker]').val() ) { 
+
+      //Validator to ensure values are not empty when creating a new task
+      if( !taskTitle ) {         
+        if( !taskDesc ) { 
+          if( !taskDate ) { 
             alert('All values are empty');    
           } else {
           alert('Name & Description are epmty');    
@@ -25,8 +25,8 @@ $(document).ready(function() {
         alert('Name is empty');    
         }
       } else {
-        if( !$('textarea[placeholder=Description]').val() ) { 
-          if( !$('input[id=datepicker]').val() ) { 
+        if( !taskDesc ) { 
+          if( !taskDate ) { 
             alert('Description and Date are empty');    
           } else {
           alert('Description is empty'); 
@@ -36,11 +36,9 @@ $(document).ready(function() {
           $('div.task-list#active').append('<div class="todo-task"></div>');
           // Append new values to the section
           $('div.todo-task:last-child').append(
-                "<div class='task-header'>" + taskTitle + "</div>" +
-                "<div class='task-description'>" + taskDesc + "</div>" +
-                "<div class='task-due'>" + taskDate + "</div>" +
-                "<button class='task-complete'>Complete</button>" +
-                "<button class='task-delete'>Delete</button>"
+              taskTitle + '<br>' + taskDesc + '<br>' + taskDate + 
+              '<br><button class="task-complete">Complete</button>' + 
+              '<button class="task-delete">Delete</button>' 
             );
         }
       }
